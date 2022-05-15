0d312a3322fe2af5168c141971e9066d62b10f34
BFrankie13 <105290208+BFrankie13@users.noreply.github.com>
2022-05-15 21:46:55 +0200

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'xo/j3vAZOPZIoCDPTFg+RInZq+gMxXVUuPaHULs1Bb0',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDdr0ULp+uMfS/7vbRkz2wboEirRl7IXEUULuqhN9QLkv7hIjSsn0V1ilT1pk0x2N4a/0UOEvjXX30lLI+X0jmyJJzjqp7WiI15l/ydr8G54ISH3x8FdrByw9S6D9acGg0qtatAbHeJLW3lrVE+7EaP3p6bMaqsiXnDtxFCB383um8BKHw4wNC13v2sNMsjDpKt74kzxIlzWS6I1cXKqDbgR4t7MiQXOu5I2xHHH0ExfszrI8EhbEp2fi4xNcuFcxZxXfBaXSuvzLE+JYJxd5p2Eijyi6ZwQFXACGayMFTZ/yKlobeFhtn1ZiJTSYTUoz+wCCc4uXeJR7NKvpf/BBy9',
 	}  
 }
